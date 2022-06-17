8d405b9e5e23caad1656b7b74e60bdfd95926e18
Annusharam <107491917+Annusharam@users.noreply.github.com>
2022-06-17 12:21:36 +0530

    Add files via upload

---
+++ MissingElementInAnArray.java
@@ -0,0 +1,16 @@
+package week.day1;
+
+import java.util.Arrays;
+
+public class MissingElementInAnArray {
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,7,6,8};
+	
+		Arrays.sort(arr);
+		int length=arr.length;
+		for(int i=0;i<length;i++) {
+			System.out.println(arr[i]);
+			int[]arr1=new int[7];
+		}
+}
+}
