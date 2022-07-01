2bdb03571ab9e291514389e7437ab0b05af75444
Annusharam <107491917+Annusharam@users.noreply.github.com>
2022-07-01 12:27:55 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,18 @@
+package Week3.Day1;
+
+import org.system.Computer;
+
+public class Desktop extends Computer
+{
+	public void desktopSize() {
+
+	}
+	public static void main(String[] args) {
+		Computer com = new Computer();
+		Desktop des = new Desktop();
+		com.computerModel();
+		des.computerModel();
+	}
+	}
+
+
