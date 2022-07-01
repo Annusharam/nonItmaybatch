9c817d5dcf0013a8ffdd7d657321a6dfa74977b4
Annusharam <107491917+Annusharam@users.noreply.github.com>
2022-07-01 13:09:24 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+package org.student;
+
+public class Student {
+	public void getStudentinfo1(String name) {
+	}
+	
+	
+	public void getStudentinfo(int id, String name)
+	{
+	}
+	public void getStudentinfo (String dept)
+	{
+	}
+
+}
---
+++ Students.java
@@ -0,0 +1,25 @@
+package org.student;
+import org.department.Department;
+
+public class Students extends Department {
+	
+	public void studentName() {
+	}
+	
+	public void studentDept() {
+	}
+	
+	public void studentId() {
+	}
+	public static void main(String[] args) {
+		Students students = new Students();
+		students.collegeCode();
+		students.collegeName();
+		students.collegeRank();
+		students.department();
+		students.studentName();
+		students.studentDept();
+		students.studentId();
+
+}
+}
